382a776c2bae0c4d0a80f93c8b6bbe5fa4aaf4f9
ashiniRathnapala19 <120577365+ashiniRathnapala19@users.noreply.github.com>
2024-03-13 12:18:45 +0530

    Finalized home page implementation
    Did final implementation and styles to home page.

--- src/Views/HomePage/HomePage.jsx
+++ src/Views/HomePage/HomePage.jsx
@@ -1,262 +1,288 @@
 import "./HomePage.css";
 import SideMenu from "../../Components/SideMenu/SideMenu";
-import S1 from "../../Assests/Slide1.jpg";
-import S2 from "../../Assests/Slide3.jpg";
-import S3 from "../../Assests/Slide4.png";
-import S4 from "../../Assests/Hiking.jpg";
+import Map from "../../Assests/worldmap.jpg";
+
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import {
-    Button,
-    Card,
-    Heading,
-    CardBody,
-    StackDivider,
-    Text,
-    Drawer,
-    DrawerOverlay,
-    DrawerContent,
-    DrawerCloseButton,
-    DrawerHeader,
-    DrawerBody,
-    Box,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
 } from "@chakra-ui/react";
+import {
+  Button,
+  Card,
+  Heading,
+  CardBody,
+  StackDivider,
+  Text,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerHeader,
+  DrawerBody,
+  Box,
+} from "@chakra-ui/react";
+
 import { ArrowRightIcon } from "@chakra-ui/icons";
 import { Alert, AlertIcon } from "@chakra-ui/react";
 import { Stack } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 
 function HomePage() {
-    const navigate = useNavigate();
-    const [selectedSlide, setSelectedSlide] = useState("radio1");
-    const handleSlideChange = (event) => {
-        setSelectedSlide(event.target.id);
-    };
-    useEffect(() => {
-        const email = localStorage.getItem("loggedInUserEmail");
-        if (!email) navigate("/login");
-    }, []);
+  const navigate = useNavigate();
+  const [selectedSlide, setSelectedSlide] = useState("radio1");
+  const handleSlideChange = (event) => {
+    setSelectedSlide(event.target.id);
+  };
+  // useEffect(() => {
+  //     const email = localStorage.getItem("loggedInUserEmail");
+  //     if (!email) navigate("/login");
+  // }, []);
 
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            const nextSlide = getNextSlideId(selectedSlide);
-            setSelectedSlide(nextSlide);
-        }, 6000);
-        return () => clearInterval(intervalId);
-    }, [selectedSlide]);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const nextSlide = getNextSlideId(selectedSlide);
+      setSelectedSlide(nextSlide);
+    }, 6000);
+    return () => clearInterval(intervalId);
+  }, [selectedSlide]);
 
-    const getNextSlideId = (currentSlide) => {
-        switch (currentSlide) {
-            case "radio1":
-                return "radio2";
-            case "radio2":
-                return "radio3";
-            case "radio3":
-                return "radio4";
-            case "radio4":
-                return "radio1";
-        }
-    };
+  const getNextSlideId = (currentSlide) => {
+    switch (currentSlide) {
+      case "radio1":
+        return "radio2";
+      case "radio2":
+        return "radio3";
+      case "radio3":
+        return "radio4";
+      case "radio4":
+        return "radio1";
+    }
+  };
 
-    const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(true);
 
-    const onClose = () => setIsOpen(false);
+  const onClose = () => setIsOpen(false);
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setIsOpen(false);
-        }, 10000);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsOpen(false);
+    }, 10000);
 
-        return () => clearTimeout(timer);
-    }, []);
+    return () => clearTimeout(timer);
+  }, []);
 
-    return (
-        <div className="bg_home">
-            <ChakraProvider>
-                <SideMenu />
-                <h1>Home Page</h1>
-                <div class="homeslider">
-                    <div class="homeslides">
-                        <input
-                            type="radio"
-                            name="radio-btn"
-                            id="radio1"
-                            checked={selectedSlide === "radio1"}
-                            onChange={handleSlideChange}
-                        />
-                        <input
-                            type="radio"
-                            name="radio-btn"
-                            id="radio2"
-                            checked={selectedSlide === "radio2"}
-                            onChange={handleSlideChange}
-                        />
-                        <input
-                            type="radio"
-                            name="radio-btn"
-                            id="radio3"
-                            checked={selectedSlide === "radio3"}
-                            onChange={handleSlideChange}
-                        />
-                        <input
-                            type="radio"
-                            name="radio-btn"
-                            id="radio4"
-                            checked={selectedSlide === "radio4"}
-                            onChange={handleSlideChange}
-                        />
+  const data = [
+    { label: "Africa Region", value: 40, color: "#5EB344" },
+    { label: "Arab Region", value: 19, color: "#FCB72A" },
+    { label: "Asia-Pacific Region", value: 30, color: "#F8821A" },
+    { label: "European Region", value: 66, color: "#E0393E" },
+    { label: "Inter-America Region", value: 36, color: "#963D97" },
+  ];
 
-                        <div class="homeslide slide1">
-                            <img src={S1} alt="" />
-                        </div>
+  return (
+    <div className="bg_home">
+      <ChakraProvider>
+        <link
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet"
+        />
+        <SideMenu />
+        <h1>Home Page</h1>
+        <Drawer isOpen={isOpen} placement="right" onClose={onClose} size="sm">
+          <DrawerOverlay />
+          <DrawerContent bgColor="#CBD5E0">
+            <DrawerCloseButton />
+            <DrawerHeader borderBottomWidth="1px" textAlign="center" size="lg">
+              Event Reminder
+            </DrawerHeader>
 
-                        <div class="homeslide slide2">
-                            <img src={S2} alt="" />
-                        </div>
-
-                        <div class="homeslide slide3">
-                            <img src={S3} alt="" />
-                        </div>
-
-                        <div class="homeslide slide4">
-                            <img src={S4} alt="" />
-                        </div>
-                    </div>
-                </div>
-
-                <div id="homecontainer">
-                    <div id="home-products-container">
-                        <a href="#">
-                            <div class="homeproduct" id="">
-                                <span className="material-symbols-outlined">
-                                    groups
-                                </span>
-                                <div className="number">1777</div>
-                                <div className="intro">Scouts</div>
-                            </div>
-                        </a>
-                        <a href="#">
-                            <div class="homeproduct" id="">
-                                <span className="material-symbols-outlined">
-                                    interactive_space
-                                </span>
-                                <div className="number">1777</div>
-                                <div className="intro">Instructors</div>
-                            </div>
-                        </a>
-
-                        <a href="#">
-                            <div class="homeproduct" id="">
-                                <span className="material-symbols-outlined">
-                                    camping
-                                </span>
-                                <div className="number">1777</div>
-                                <div className="intro">Events nearby</div>
-                            </div>
-                        </a>
-                        <a href="mcq">
-                            <div class="homeproduct" id="">
-                                <span className="material-symbols-outlined">
-                                    description
-                                </span>
-                                <div className="number">1777</div>
-                                <div className="intro">MCQ'S</div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <Drawer
-                    isOpen={isOpen}
-                    placement="right"
-                    onClose={onClose}
-                    size="sm"
+            <DrawerBody>
+              <Alert status="info" variant="left-accent" marginTop={10}>
+                <AlertIcon />A new event has been added !
+              </Alert>
+              <Card marginTop={20}>
+                <CardBody>
+                  <Stack
+                    divider={<StackDivider />}
+                    spacing="7"
+                    borderColor="black"
+                  >
+                    <Box style={{ marginBottom: "20px" }}>
+                      <Heading
+                        size="xs"
+                        textTransform="uppercase"
+                        color="blue.500"
+                      >
+                        Event Name
+                      </Heading>
+                      <Text pt="2" fontSize="sm">
+                        View a summary of all your clients over the last month.
+                      </Text>
+                    </Box>
+                    <Box style={{ marginBottom: "20px" }}>
+                      <Heading
+                        size="xs"
+                        textTransform="uppercase"
+                        color="green.500"
+                      >
+                        Date
+                      </Heading>
+                      <Text pt="2" fontSize="sm">
+                        Check out the overview of your clients.
+                      </Text>
+                    </Box>
+                    <Box>
+                      <Heading
+                        size="xs"
+                        textTransform="uppercase"
+                        color="purple.500"
+                      >
+                        Location
+                      </Heading>
+                      <Text pt="2" fontSize="sm">
+                        See a detailed analysis of all your business clients.
+                      </Text>
+                    </Box>
+                  </Stack>
+                </CardBody>
+              </Card>
+              <Link to="/event">
+                <Button
+                  rightIcon={<ArrowRightIcon />}
+                  colorScheme="blackAlpha"
+                  variant="solid"
+                  marginLeft={130}
+                  marginTop={100}
                 >
-                    <DrawerOverlay />
-                    <DrawerContent bgColor="#CBD5E0">
-                        <DrawerCloseButton />
-                        <DrawerHeader
-                            borderBottomWidth="1px"
-                            textAlign="center"
-                            size="lg"
-                        >
-                            Event Reminder
-                        </DrawerHeader>
+                  See more
+                </Button>
+              </Link>
+            </DrawerBody>
+          </DrawerContent>
+        </Drawer>
+        <div class="hm_content">
+          <div class="hm_card">
+            <div class="hm_icon">
+              <i class="material-icons md-36">groups</i>
+            </div>
 
-                        <DrawerBody>
-                            <Alert
-                                status="info"
-                                variant="left-accent"
-                                marginTop={10}
-                            >
-                                <AlertIcon />A new event has been added !
-                            </Alert>
-                            <Card marginTop={20}>
-                                <CardBody>
-                                    <Stack
-                                        divider={<StackDivider />}
-                                        spacing="7"
-                                        borderColor="black"
-                                    >
-                                        <Box style={{ marginBottom: "20px" }}>
-                                            <Heading
-                                                size="xs"
-                                                textTransform="uppercase"
-                                                color="blue.500"
-                                            >
-                                                Event Name
-                                            </Heading>
-                                            <Text pt="2" fontSize="sm">
-                                                View a summary of all your
-                                                clients over the last month.
-                                            </Text>
-                                        </Box>
-                                        <Box style={{ marginBottom: "20px" }}>
-                                            <Heading
-                                                size="xs"
-                                                textTransform="uppercase"
-                                                color="green.500"
-                                            >
-                                                Date
-                                            </Heading>
-                                            <Text pt="2" fontSize="sm">
-                                                Check out the overview of your
-                                                clients.
-                                            </Text>
-                                        </Box>
-                                        <Box>
-                                            <Heading
-                                                size="xs"
-                                                textTransform="uppercase"
-                                                color="purple.500"
-                                            >
-                                                Location
-                                            </Heading>
-                                            <Text pt="2" fontSize="sm">
-                                                See a detailed analysis of all
-                                                your business clients.
-                                            </Text>
-                                        </Box>
-                                    </Stack>
-                                </CardBody>
-                            </Card>
-                            <Link to="/event">
-                                <Button
-                                    rightIcon={<ArrowRightIcon />}
-                                    colorScheme="blackAlpha"
-                                    variant="solid"
-                                    marginLeft={130}
-                                    marginTop={100}
-                                >
-                                    See more
-                                </Button>
-                            </Link>
-                        </DrawerBody>
-                    </DrawerContent>
-                </Drawer>
-            </ChakraProvider>
+            <p class="hm_title">SCOUTS</p>
+            <p class="hm_text">Click to see or edit your profile page.</p>
+          </div>
+
+          <div class="hm_card">
+            <div class="hm_icon">
+              <i class="material-icons md-36">person</i>
+            </div>
+            <p class="hm_title">INSTRUCTORS</p>
+            <p class="hm_text">Check all your favourites in one place.</p>
+          </div>
+
+          <div class="hm_card">
+            <div class="hm_icon">
+              <i class="material-icons md-36">event</i>
+            </div>
+            <p class="hm_title">EVENTS</p>
+            <p class="hm_text">Add or change your contacts and links.</p>
+          </div>
+          <a href="/mcq">
+            <div class="hm_card">
+              <div class="hm_icon">
+                <i class="material-icons md-36">description</i>
+              </div>
+              <p class="hm_title">MCQ</p>
+              <p class="hm_text">Enhance your scouting knowledge</p>
+            </div>
+          </a>
         </div>
-    );
+
+        <div className="simple-bar-chart">
+          {data.map((item, index) => (
+            <div
+              key={index}
+              className="item"
+              style={{ "--clr": item.color, "--val": item.value }}
+            >
+              <div className="label">{item.label}</div>
+              <div className="value">{`${item.value}`}</div>
+            </div>
+          ))}
+        </div>
+
+        <TableContainer marginTop={-380}>
+          <Table
+            bgColor="white"
+            variant="striped"
+            colorScheme="blue"
+            size="lg"
+            width={300}
+            marginLeft={950}
+          >
+            <TableCaption textColor="black" fontWeight={700}>
+              Scouting distribution around the world
+            </TableCaption>
+            <Thead>
+              <Tr>
+                <Th bgColor="#1E90FF" textColor="black">
+                  Region
+                </Th>
+                <Th bgColor="#1E90FF" textColor="black">
+                  Number of organization
+                </Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td>Africa Region</Td>
+                <Td textAlign="center">40</Td>
+              </Tr>
+              <Tr>
+                <Td>Arab Region</Td>
+                <Td textAlign="center">19</Td>
+              </Tr>
+              <Tr>
+                <Td>Asia-Pacific Region</Td>
+                <Td textAlign="center">30</Td>
+              </Tr>
+              <Tr>
+                <Td>European Region</Td>
+                <Td textAlign="center">66</Td>
+              </Tr>
+              <Tr>
+                <Td>Interamerica Region</Td>
+                <Td textAlign="center">36</Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </TableContainer>
+
+        <div className="hm_ftr">
+          <img src={Map} id="map" />
+          <ul class="bargraph">
+            <li class="reddeep">Africa Region</li>
+            <li class="redpink">Arab Region</li>
+            <li class="orange">Asia-Pacific Region</li>
+            <li class="yellow">European Region</li>
+            <li class="greenbright">Inter-America Region</li>
+            <br></br>
+            <lable id="grp_lbl">
+              Number of scouts around the world (Million)
+            </lable>
+          </ul>
+        </div>
+      </ChakraProvider>
+    </div>
+  );
 }
 
 export default HomePage;
